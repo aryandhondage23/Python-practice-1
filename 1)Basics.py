5d26a54a78dfd4866eda59cc614c6a9a7f370012
Aryan Dhondage <aryandhondage@gmail.com>
2026-06-03 22:22:48 +0530

    Create 1)Basics.py

---
+++ 1)Basics.py
@@ -0,0 +1,15 @@
+print("hello")
+
+#Modules and Pip
+#Module is like a code library which can be used to borrow code written by somebody else in our python program. There are two types of modules in python:
+#Built in Modules - These modules are ready to import and use and ships with the python interpreter. there is no need to install such modules explicitly. External Modules - These modules are imported from a third party file or can be installed using a package manager like pip or conda. Since this code is written by someone else, we can install different versions of a same module with time.
+
+import pandas
+import numpy
+
+#\n = use for brake line and start the remainin sentence from new line
+print("hello i am aryan \niam student")
+
+# sep= to seprate the all values
+print("haello",6,7, sep="-", end="next\n")
+print("aryan")
